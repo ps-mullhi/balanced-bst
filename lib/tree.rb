8dacebe998fb4191d59dc05fa4233c3cd87d0997
Puneet Singh <199150760+ps-mullhi@users.noreply.github.com>
2025-03-15 12:34:03 -0700

    implemented insert

--- lib/tree.rb
+++ lib/tree.rb
@@ -7,6 +7,22 @@ class Tree
     @root = build_tree(prepped_array, 0, prepped_array.length - 1)
   end
 
+  def insert(new_value, node = @root)
+    return if new_value == node.value #not allowing duplicates
+
+    if node.left.nil? && new_value < node.value
+      node.left = Node.new(new_value)
+    elsif node.right.nil? && new_value > node.value
+      node.right = Node.new(new_value)
+    else
+      if new_value < node.value
+        insert(new_value, node.left)
+      else
+        insert(new_value, node.right)
+      end
+    end
+  end
+
   # not my method. Odin Project student shared on discord, and linked in project description
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
 require_relative 'lib/tree.rb'
 
-tree = Tree.new([5,5,5,1,3,2,2,3,4])
+tree = Tree.new([1,3,5,7,8,9])
 tree2 = Tree.new([1,2,3,4,5,6,7,8,9])
 
-p tree
+# p tree
 tree.pretty_print
-tree2.pretty_print
+tree.insert(6)
+tree.pretty_print
+
+
+# tree2.pretty_print
